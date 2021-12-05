7aa5f93545c6e3db10255b5c2f6dd29d5e86d523
Kowshik Das <kowshikdas@Kowshiks-MacBook-Pro.local>
2021-12-05 02:51:01 +0100

    V1 - Third commit on UserLoginAndRegistration Project on 04.12.2021

--- UserLoginAndRegistration/src/main/java/com/example/UserLoginAndRegistration/service/RegistrationService.java
+++ UserLoginAndRegistration/src/main/java/com/example/UserLoginAndRegistration/service/RegistrationService.java
@@ -1,11 +1,34 @@
 package com.example.UserLoginAndRegistration.service;
 
+import com.example.UserLoginAndRegistration.email.EmailValidator;
+import com.example.UserLoginAndRegistration.model.AppUser;
+import com.example.UserLoginAndRegistration.model.AppUserRole;
 import com.example.UserLoginAndRegistration.model.RegistrationRequest;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RegistrationService {
+
+    private final AppUserService appUserService;
+    private final EmailValidator emailValidator;
+
     public String register(RegistrationRequest request) {
-        return "works";
+        boolean isEmailValid = emailValidator.test(request.getEmail());
+
+        if(!isEmailValid) {
+            throw new IllegalStateException("Email is not valid.");
+        }
+
+        return appUserService.signUpUser(
+                new AppUser(
+                        request.getFirstName(),
+                        request.getLastName(),
+                        request.getEmail(),
+                        request.getPassword(),
+                        AppUserRole.USER
+                )
+        );
     }
 }
